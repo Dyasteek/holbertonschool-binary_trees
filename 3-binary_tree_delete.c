79f1e7cc6a6b840dfc8ff83fe14ac84b5e376d72
Dyasteek <lawarsenik@gmail.com>
2025-08-06 13:03:49 +0000

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,16 +3,20 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
- *
+ * binary_tree_delete - Deletes all nodes of the binary tree.
+ * @tree: Reference to the main tree.
+ * Return: Nothing.
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	free(tree);
 	if (tree == NULL)
 		return;
+
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+	free(tree);
 }
